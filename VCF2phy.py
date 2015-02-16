42790a7bd9fbb9106b874d32496a6043217b6d5a
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-02-16 15:00:47 +0000

    Added LICENSE headers.

--- VCF2phy.py
+++ VCF2phy.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+# Copyright 2015 Francisco Pina Martins <f.pinamartins@gmail.com>
+# This file is part of VCP2phy.
+# VCP2phy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# VCP2phy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with VCP2phy. If not, see <http://www.gnu.org/licenses/>.
 
 from sys import argv
 
