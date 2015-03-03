c0355c8dd4f6133e461a5a3d66ca8aefda41befe
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-03-03 12:42:12 +0000

    Better PEP8 conformation.

--- structure_by_threads2.py
+++ structure_by_threads2.py
@@ -15,55 +15,57 @@
 # You should have received a copy of the GNU General Public License
 # along with structure_by_threads2. If not, see <http://www.gnu.org/licenses/>.
 
-#Usage: python3 structure_pipe.py K reps infile outpath num_of_threads
-#Where "K" is the number of "Ks" to test, "reps" is the number of replicates
-#and num_of_threads is the number of threads.
+# Usage: python3 structure_pipe.py K reps infile outpath num_of_threads
+# Where "K" is the number of "Ks" to test, "reps" is the number of replicates
+# and num_of_threads is the number of threads.
 
 import subprocess
 from multiprocessing import Pool
 
-##Declare some global variables:
-#Where is structure?
+# Where is structure?
 structure_bin = "/opt/structure/bin/structure"
 
 
-def RunProgram(iterations):
-    """Runs external programs and deals with their output."""
+def runprogram(iterations):
+    """Run each structure job."""
     K, rep_num = iterations
-    program_stdout = []
-    cli = [structure_bin, "-K", str(K), "-i", infile, "-o", outpath +  "/K" + str(K) + "_rep" + str(rep_num)]
-    try:
-        program = subprocess.Popen(cli, bufsize=64,shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
-        for lines in program.stdout:
-            lines = lines.decode("utf-8").strip()
-            print(lines)
-            program_stdout.append(lines)
-    except:
-        quit("\nERROR:Program not found... exiting. Check your configuration file.\n")
+    cli = [structure_bin, "-K", str(K), "-i", infile, "-o", outpath + "/K"
+           + str(K) + "_rep" + str(rep_num)]
+    print("Running: " + " ".join(cli))
+    program = subprocess.Popen(cli, bufsize=64, shell=False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+
+    # This loop does nothing, but seems to be required for the program to fully
+    # run all the jobs...
+    for lines in program.stdout:
+        pass
 
 
 def structure_threader(Ks, replicates, threads):
+    """Do the threading book-keeping to spawn jobs at the asked rate."""
     pool = Pool(threads)
     jobs = []
-    # Ugly, ugly code. TODO: change to someting more elegant.
+    # Ugly, ugly code. But since we are just dealing with counts here, we can
+    # just leave it like that...
     for k in Ks:
         for rep in replicates:
             jobs.append((k, rep))
-    
-    pool.map(RunProgram, jobs)
+
+    pool.map(runprogram, jobs)
     pool.close()
     pool.join()
+    print("All jobs finished.")
 
 
 if __name__ == "__main__":
     from sys import argv
-    #Number of K
+    # Number of K
     Ks = range(1, int(argv[1]) + 1)
-    #Number of replicates
+    # Number of replicates
     replicates = range(1, int(argv[2]) + 1)
     infile = argv[3]
     outpath = argv[4]
     threads = int(argv[5])
-    
+
     structure_threader(Ks, replicates, threads)
