a6882aa94c77749a92c39df4ec9115d3154c261c
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-05-21 22:20:05 +0100

    Changed copyright. Can't take credit for this one. =-)

--- compare_pairs.py
+++ compare_pairs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright 2015 Francisco Pina Martins <f.pinamartins@gmail.com>
+# Copyright 2015 Diogo Silva <o.diogo.silva@gmail.com>
 # This file is part of Loci_counter.
 # Loci_counter is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
