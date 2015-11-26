54f7dc5456b741aeb57a15eeff89620ec2880248
ODiogoSilva <o.diogosilva@gmail.com>
2015-11-26 10:21:39 +0000

    Added SNP distribution plot

--- vcftools_stats.py
+++ vcftools_stats.py
@@ -22,6 +22,7 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 # Setting plot style
 plt.style.use("ggplot")
@@ -37,6 +38,10 @@ parser.add_argument("--weir-fst", dest="weir_fst", help="Parse a Weir FST"
 parser.add_argument("--singleton", dest="singleton", const=True,
                     action="store_const", help="Parse a singleton/doubleton"
                     " output file")
+parser.add_argument("--alternative-snps", dest="altsnp", const=True,
+                    action="store_const", help="Parse a VCF file and plot"
+                    " the distribution of the number of alternative allele"
+                    " SNPs per taxa")
 
 arg = parser.parse_args()
 
@@ -123,6 +128,76 @@ def singletons(infile):
     plt.savefig("singleton_distribution.svg")
 
 
+def alternative_snp_distribution(vcf_file):
+    """
+    Parses a VCF file and plots the frequency of the number of alternative SNPs
+    for each taxon
+    """
+
+    vcf_fh = open(vcf_file)
+
+    # Maximum number of total alternative SNPs for plotting
+    max_alt = 0
+
+    # Skip header
+    for line in vcf_fh:
+        if line.startswith("##"):
+            pass
+        elif line.startswith("#CHROM"):
+            # Get taxa names and positions
+            taxa_names = line.strip().split()[9:]
+            storage = dict((x, defaultdict(int)) for x in taxa_names)
+        else:
+            fields = line.strip().split()
+            # Get genotypes
+            genotypes = [x.split(":")[0] for x in fields[9:]]
+
+            # Get total number of alternative SNPs
+            alt_num = 0
+            for i in genotypes:
+                if "1" in i:
+                    alt_num += 1
+
+            # Update max alternative alleles
+            if alt_num > max_alt:
+                max_alt = alt_num
+
+            # Get number of alternative SNPs for each taxa
+            for p, i in enumerate(genotypes):
+                if "1" in i:
+                    # Add 1 to the taxa counter for the number of alt_num
+                    storage[taxa_names[p]][alt_num] += 1
+
+    # Prepare data for plotting
+    plot_data = []
+    for i in range(1, max_alt + 1):
+        temp_list = []
+        for taxon in taxa_names:
+            temp_list.append(storage[taxon][i])
+        plot_data.append(temp_list)
+
+    # Creating plot
+    fig, ax = plt.subplots()
+
+    colors = plt.cm.jet(np.linspace(0, 2, len(taxa_names)))
+
+    ind = np.arange(len(taxa_names))
+    y_offset = np.array([0.0] * len(taxa_names))
+
+    plots = []
+    w = 0.80
+    for i in range(len(plot_data)):
+        p = plt.bar(ind, plot_data[i], w, bottom=y_offset, color=colors[i])
+        y_offset = y_offset + plot_data[i]
+        plots.append(p)
+
+    plt.xticks(ind + w/2., taxa_names, rotation=45, ha="right")
+
+    plt.legend(plots, range(1, max_alt + 1))
+
+    fig.tight_layout()
+    plt.savefig("Alternative_SNPs_distribution.png")
+
 def main():
 
     # Arguments
@@ -133,5 +208,8 @@ def main():
 
     if arg.singleton:
         singletons(infile)
+
+    if arg.altsnp:
+        alternative_snp_distribution(infile)
 
 main()
