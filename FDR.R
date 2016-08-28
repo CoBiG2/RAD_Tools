46f1423c754b871082360b303201e9df86965ce3
Francisco Pina Martins <f.pinamartins@gmail.com>
2016-08-28 01:36:51 +0100

    Corrected typo in license info.

--- FDR.R
+++ FDR.R
@@ -1,18 +1,18 @@
 #!/usr/bin/Rscript
 # Copyright 2015 Francisco Pina Martins <f.pinamartins@gmail.com>
-# This file is part of FRD.R.
-# FRD.R is free software: you can redistribute it and/or modify
+# This file is part of FDR.R.
+# FDR.R is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# FRD.R is distributed in the hope that it will be useful,
+# FDR.R is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with FRD.R. If not, see <http://www.gnu.org/licenses/>.
+# along with FDR.R. If not, see <http://www.gnu.org/licenses/>.
 
 
 #Usage: Rscript FDR.R infile.csv outfile.csv
