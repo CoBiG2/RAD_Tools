75d60f734fd04199f51fff33caf426527f6d4661
Francisco Pina Martins <f.pinamartins@gmail.com>
2018-10-15 17:11:12 +0100

    Program now works.

--- segregating_loci_finder.py
+++ segregating_loci_finder.py
@@ -18,6 +18,8 @@
 # loci that segregate both groups
 
 import re
+from collections import Counter, OrderedDict
+
 
 def vcf_parser(vcf_filename, group_split_point):
     """
@@ -25,15 +27,69 @@ def vcf_parser(vcf_filename, group_split_point):
     """
     infile = open(vcf_filename, 'r')
     loci = {}
+    group_split_point = int(group_split_point)
     for lines in infile:
         if lines.startswith("#"):  # Skip headers
-            pass
+            if lines.startswith("#CHROM"):  # Group checker lines
+                lines = lines.split()
+                data = lines[9:]
+                groups = [data[:group_split_point],
+                          data[group_split_point:]]
+                print(groups)
         else:
             lines = lines.split()
             locus = lines[0]
-            data = lines[8:]
-            groups = []
-            freqs = []
-            groups.append(data[:group_split_point])
-            groups.append(data[group_split_point:])
-            abs_freqs = [re.match(".*:", x) for x in groups[0]]
+            data = lines[9:]
+            groups = [data[:group_split_point], data[group_split_point:]]
+            gr_freqs = [get_freqs(x) for x in groups]
+            loci[locus] = gr_freqs
+
+    return loci
+
+
+def get_freqs(vcf_data):
+    """
+    Gets relative frequencies from VCF data
+    """
+    abs_freqs = [re.match(".*?:", x).group(0)[:-1] for x in vcf_data]
+    dummy_freqs = {"0/0": 0, "0/1": 0, "1/0": 0, "1/1": 0, "./.": 0}
+    rel_freqs = Counter(abs_freqs)
+    try:
+        mvs = rel_freqs.pop("./.")
+    except KeyError:
+        mvs = 0
+    dummy_freqs.update(Counter(abs_freqs))
+    rel_freqs = dummy_freqs
+    rel_freqs["0/1"] += rel_freqs.pop("1/0")
+    try:
+        non_missing = len(abs_freqs) - mvs
+        rel_freqs = {k: v/non_missing for k, v in rel_freqs.items()}
+    except ZeroDivisionError:
+        rel_freqs = None
+    # print(rel_freqs)
+
+    return rel_freqs
+
+
+def segregating_freqs(loci):
+    """
+    Defines wether a locus segregates the two groups
+    For now only works with full segregation
+    """
+    segregators = []
+    for locus, data in loci.items():
+        try:
+            segregators += [locus for k, v in data[0].items()
+                            if (data[1][k] == 0 and v == 1)
+                            or (data[1][k] == 1 and v == 0)]
+        except AttributeError:
+            pass
+
+    return segregators
+
+
+if __name__ == "__main__":
+    from sys import argv
+    seg_loci = vcf_parser(argv[1], argv[2])
+    for i in segregating_freqs(seg_loci):
+        print(i)
