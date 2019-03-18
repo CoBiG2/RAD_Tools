c3dbcbe5af2378333b7af188628b91e9565e4902
Duarte Balata <duarte.balata@hotmail.com>
2019-03-18 13:17:35 +0000

    Added the possibility to choose the percentage of missing data allowed in a group of replicates.

--- filter_replicates_vcf.py
+++ filter_replicates_vcf.py
@@ -5,7 +5,6 @@ Created on Thu Mar 14 14:04:41 2019
 
 @author: duartb
 """
-from statistics import mode
 import argparse
 
 parser = argparse.ArgumentParser(prog='python3')
@@ -16,9 +15,17 @@ parser.add_argument("input_replicates", metavar="replicates.txt", type=str,
                         help="Input file with all the replicates' names")
 parser.add_argument("output", metavar="output.vcf", type=str,
                         help="Output filtered VCF")
+parser.add_argument("-m","--missing_data_percentage", metavar="25",type=int, default=25,
+                        help= "Maximum percentage of missing values allowed in a set of replicates (default=25%")
 arguments = parser.parse_args()
 
+missing_ratio= arguments.missing_data_percentage/100
+
 def filter_replicate_vcf(vcf_input,replicates_input,vcf_output):
+    
+    non_missing=0
+    missing=0 
+    copies=0
     
     vcf_file = open(vcf_input,"r")
     replicates_file = open(replicates_input,"r")
@@ -56,13 +63,16 @@ def filter_replicate_vcf(vcf_input,replicates_input,vcf_output):
     output.write(new_header + "\n")
     
     genes=[]
+    deleted_loci=0
+    loci=0
     
     for line in vcf_file:
-        
+         
         write= True
         line = line.split()
         ind_dictio= {}    
         ind=0
+        loci +=1
         
         # iteares over the columns of a line in a vcf file
         for i in range(9,len(line)):
@@ -70,14 +80,14 @@ def filter_replicate_vcf(vcf_input,replicates_input,vcf_output):
             # creates list of the genes in N columns, where N is the number of replicates used for each sample
             if len(genes) == replicate_number:
                 # if an individual has contraditory information between its replicates the present snp is skipped
-                if len(set(genes)) > 1 and "./." not in set(genes):
-                    write= False
-                    pass
+                if (len(set(genes)) > 1 and "./." not in set(genes)) or len(set(genes)) > 2:
+                    write = False
                 # generates dictiorary with genotypes of all individuals in present line
                 else:
-                    try:
-                        ind_dictio[individuals[ind]]= mode(genes)
-                    except:
+                    if genes.count("./.") <= len(genes) * missing_ratio:
+                        while "./." in genes: genes.remove("./.")
+                        ind_dictio[individuals[ind]]= genes[0]
+                    else:
                         ind_dictio[individuals[ind]]= "./."
                 # resets list of replicate genes       
                 genes=[]
@@ -85,11 +95,21 @@ def filter_replicate_vcf(vcf_input,replicates_input,vcf_output):
                 ind += 1
         
         # line is writen only if none of the replicates had contraditory information
-        if write == True:        
+        if write == False:
+            deleted_loci +=1
+        
+        if write == True:
             new_line= line[0:9] + list(ind_dictio.values())
             line_write= "\t".join(new_line)
             output.write(line_write + "\n")
+            
+            non_missing += (list(ind_dictio.values()).count("0/1") + list(ind_dictio.values()).count("1/1") + list(ind_dictio.values()).count("0/0"))
+            missing += list(ind_dictio.values()).count("./.")
+            copies += len(ind_dictio.values())
     
     output.close()
+    
+    print("\n"+ str(deleted_loci) + " loci out of " + str(loci) + " were removed due to contraditory information between replicates.")
+    print(str(missing) + " Snp's out of " + str(non_missing + missing) + " were considered missing data." + "\n")
     
 filter_replicate_vcf(arguments.input_vcf,arguments.input_replicates,arguments.output)
